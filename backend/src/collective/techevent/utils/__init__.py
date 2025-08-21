d01a91f96015395e63e17ab564b1df4c08b3f9a3
Érico Andrei <ericof@gmail.com>
2025-08-21 17:00:47 -0300

    Fix start and end datetime not being timezone aware in IScheduleSlot and IEventRoot (Fixes #11) (#12)

--- backend/src/collective/techevent/utils/__init__.py
+++ backend/src/collective/techevent/utils/__init__.py
@@ -1,53 +1,13 @@
-from Acquisition import aq_parent
 from collective.techevent import logger
-from collective.techevent.behaviors.tech_event import Settings
-from collective.techevent.content.sponsors.sponsor_level import SponsorLevel
-from collective.techevent.content.sponsors.sponsors_db import SponsorsDB
-from collective.techevent.interfaces import IEventRoot
+from collective.techevent.utils.event import find_event_root
+from collective.techevent.utils.event import find_sponsors_db
+from collective.techevent.utils.event import get_sponsorship_benefits
+from collective.techevent.utils.event import sponsor_levels
 from plone import api
 from plone.dexterity.content import DexterityContent
 from plone.dexterity.fti import DexterityFTI
 from plone.dexterity.interfaces import IDexterityFTI
 from zope.component import queryUtility
-
-
-def find_event_root(context: DexterityContent) -> Settings:
-    """Find nearest event root."""
-    if IEventRoot.providedBy(context):
-        return context
-    else:
-        return find_event_root(aq_parent(context))
-
-
-def find_sponsors_db(context: DexterityContent) -> SponsorsDB | None:
-    """Find nearest sponsors db."""
-    event_root = find_event_root(context)
-    brains = api.content.find(event_root, portal_type="SponsorsDB")
-    if brains:
-        sponsors_db = brains[0].getObject()
-        return sponsors_db
-
-
-def sponsor_levels(context: DexterityContent) -> list[tuple[SponsorLevel, dict]]:
-    items = []
-
-    if sponsors_db := find_sponsors_db(context):
-        brains = api.content.find(
-            sponsors_db, portal_type="SponsorLevel", sort_on="getObjPositionInParent"
-        )
-        for brain in brains:
-            level = brain.getObject()
-            level_images = brain.image_scales
-            items.append((level, level_images))
-    return items
-
-
-def get_sponsorship_benefits(context: DexterityContent) -> list[dict[str, str]]:
-    """Return a list of sponsorship benefits."""
-    benefits = []
-    if sponsors_db := find_sponsors_db(context):
-        benefits = sponsors_db.benefits
-    return benefits
 
 
 PERMISSIONS = [
@@ -157,3 +117,14 @@ def setup_tech_event_users(portal: DexterityContent):
         if fti.global_allow:
             fti.global_allow = False
             logger.info("Disabled global_allow for Attendees")
+
+
+__all__ = [
+    "find_event_root",
+    "find_sponsors_db",
+    "get_sponsorship_benefits",
+    "modify_permission",
+    "setup_tech_event",
+    "setup_tech_event_users",
+    "sponsor_levels",
+]
--- backend/src/collective/techevent/utils/dates.py
+++ backend/src/collective/techevent/utils/dates.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 from datetime import timedelta
+from datetime import tzinfo
+from plone import api
+from plone.dexterity.content import DexterityContent
+from zope.interface import provider
+from zope.schema.interfaces import IContextAwareDefaultFactory
+
+import pytz
 
 
 def get_iso_dates_between(start: datetime, end: datetime) -> list[str]:
-    """
-    Returns a list of ISO date strings (YYYY-MM-DD) for each day between start and end.
+    """Return a list of ISO date (YYYY-MM-DD) for each day between
+       start and end.
 
     :param start: Start datetime (inclusive)
     :param end: End datetime (inclusive)
@@ -19,3 +26,32 @@ def get_iso_dates_between(start: datetime, end: datetime) -> list[str]:
 
     delta = (end_date - start_date).days
     return [(start_date + timedelta(days=i)).isoformat() for i in range(delta + 1)]
+
+
+def portal_timezone() -> tzinfo:
+    """Return the portal timezone for the given context."""
+    tz = pytz.timezone(api.portal.get_registry_record("plone.portal_timezone"))
+    return tz
+
+
+def localized_now(context: DexterityContent | None = None) -> datetime:
+    """Return the current datetime localized to the default timezone."""
+    tz = portal_timezone()
+    return datetime.now(tz).replace(microsecond=0)
+
+
+def _default_start(context: DexterityContent | None = None) -> datetime:
+    """Provide default start for the form."""
+    return localized_now(context)
+
+
+@provider(IContextAwareDefaultFactory)
+def default_start(context: DexterityContent | None = None) -> datetime:
+    """Provide default start for the form."""
+    return _default_start(context)
+
+
+@provider(IContextAwareDefaultFactory)
+def default_end(context: DexterityContent | None = None) -> datetime:
+    """Provide default end for the form."""
+    return _default_start(context) + timedelta(hours=1)
